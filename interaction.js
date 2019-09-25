181c0a26616478602642ed9153d3a7231e28b8ca
yuxinwang <sylviawang1109@qq.com>
2019-09-25 12:42:43 +0800

    test-1

---
+++ interaction.js
@@ -0,0 +1,12 @@
+function setup(){
+    createCanvas(600, 400);
+   // Canvas.position(0, 0);
+}
+
+function draw(){
+    background(221,223,421);
+}
+
+function mousePressed(){
+    createP("My dream school is Royal College of Art");
+}
